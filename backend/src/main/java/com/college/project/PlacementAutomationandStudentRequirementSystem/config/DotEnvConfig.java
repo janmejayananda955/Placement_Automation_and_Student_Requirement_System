32e4b7510bf70592150d63aecb700676e101f15a
Janmejaya <149013040+janmejayananda955@users.noreply.github.com>
2026-03-28 01:59:52 +0530

    Update DotEnvConfig.java

--- backend/src/main/java/com/college/project/PlacementAutomationandStudentRequirementSystem/config/DotEnvConfig.java
+++ backend/src/main/java/com/college/project/PlacementAutomationandStudentRequirementSystem/config/DotEnvConfig.java
@@ -3,7 +3,10 @@ package com.college.project.PlacementAutomationandStudentRequirementSystem.confi
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+
+@Profile("local")
 @Configuration
 public class DotEnvConfig {
 
